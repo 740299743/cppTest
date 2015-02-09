213e8595bb3a1f79e87c0d3a056ec36c8a350c10
pactera <pactera@pacteras-Mac-mini.local>
2015-02-09 13:49:07 +0800

    Initial Commit

---
+++ cppTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cppTest
+//
+//  Created by pactera on 2/9/15.
+//  Copyright (c) 2015 pactera. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
