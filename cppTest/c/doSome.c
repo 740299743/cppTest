2c9dfeaec9519473d0a6e976ee37b180ad4a31d1
740299743@qq.com <740299743@qq.com>
2015-02-12 16:45:52 +0800

    dosome

---
+++ cppTest/c/doSome.c
@@ -0,0 +1,9 @@
+//
+//  doSome.c
+//  cppTest
+//
+//  Created by pactera on 2/12/15.
+//  Copyright (c) 2015 pactera. All rights reserved.
+//
+
+#include "doSome.h"
---
+++ cppTest/c/doSome.h
@@ -0,0 +1,14 @@
+//
+//  doSome.h
+//  cppTest
+//
+//  Created by pactera on 2/12/15.
+//  Copyright (c) 2015 pactera. All rights reserved.
+//
+
+#ifndef __cppTest__doSome__
+#define __cppTest__doSome__
+
+#include <stdio.h>
+
+#endif /* defined(__cppTest__doSome__) */
